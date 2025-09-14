3f1b96425ac2e0f3c8ce4c2de287946f5e15a9fd
Amrsamy19 <amrsamy622@gmail.com>
2025-09-14 13:13:50 +0300

    refactor: improve code formatting and consistency in Popular, Search, and Trending components

--- src/components/actors/index.tsx
+++ src/components/actors/index.tsx
@@ -1,45 +1,56 @@
-import { useEffect, useState } from "react"
-import { IActor, IMovie } from "@/components/interfaces"
+import { useEffect, useState } from "react";
+import { IActor, IMovie } from "@/components/interfaces";
 
 export const Actors = () => {
-	const [actors, setActors] = useState<Array<IActor>>([])
+  const [actors, setActors] = useState<Array<IActor>>([]);
 
-	useEffect(() => {
-		(async () => {
-			try {
-				const response = await fetch("https://api.themoviedb.org/3/person/popular?api_key=0544e732b88a2287b7288288339c30cf");
-				if (!response.ok) throw Error("Can't get the response");
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/person/popular?api_key=0544e732b88a2287b7288288339c30cf"
+        );
+        if (!response.ok) throw Error("Can't get the response");
 
-				const json = await response.json();
-				setActors(json.results)
-			} catch (e) {
-				console.log(e);
-			}
-		})()
-	}, [])
+        const json = await response.json();
+        setActors(json.results);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  }, []);
 
-	return (
-		<>
-			<section>
-				<p className="font-bold text-5xl mx-10 mb-10">Popular People</p>
-				<div className="grid grid-cols-6 gap-y-2">
-					{actors.map((actor: IActor) => {
-						return actor.profile_path && (
-							<a href={`/person/${actor.id}`} className="mx-10 hover:underline">
-								<img className="rounded-xl w-full mb-2" src={`https://image.tmdb.org/t/p/w780${actor.profile_path}`} />
-								<div className="flex flex-col justify-between">
-									<p className="text-md font-bold">
-										{actor.name}
-									</p>
-									<p className="text-md w-40 truncate font-medium text-gray-500/70">
-										{actor.known_for.map((movie: IMovie) => movie.name || movie.title).join(', ')}
-									</p>
-								</div>
-							</a>
-						)
-					})}
-				</div>
-			</section>
-		</>
-	)
-}
+  return (
+    <>
+      <section>
+        <p className="font-bold text-5xl mx-10 mb-10">Popular People</p>
+        <div className="grid grid-cols-6 gap-y-2">
+          {actors.map((actor: IActor) => {
+            return (
+              actor.profile_path && (
+                <a
+                  href={`/person/${actor.id}`}
+                  key={actor.id.toString()}
+                  className="mx-10 hover:underline"
+                >
+                  <img
+                    className="rounded-xl w-full mb-2"
+                    src={`https://image.tmdb.org/t/p/w780${actor.profile_path}`}
+                  />
+                  <div className="flex flex-col justify-between">
+                    <p className="text-md font-bold">{actor.name}</p>
+                    <p className="text-md w-40 truncate font-medium text-gray-500/70">
+                      {actor.known_for
+                        .map((movie: IMovie) => movie.name || movie.title)
+                        .join(", ")}
+                    </p>
+                  </div>
+                </a>
+              )
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
--- src/components/movies/Trending.tsx
+++ src/components/movies/Trending.tsx
@@ -1,60 +1,71 @@
-import { useEffect, useState } from "react"
-import { IMovie } from "@/components/interfaces"
+import { useEffect, useState } from "react";
+import { IMovie } from "@/components/interfaces";
 
 export const Trending = () => {
-	const [trending, setTrending] = useState<Array<IMovie>>([]);
-	const [topTrending, setTopTrending] = useState<IMovie>();
+  const [trending, setTrending] = useState<Array<IMovie>>([]);
+  const [topTrending, setTopTrending] = useState<IMovie>();
 
-	useEffect(() => {
-		(async () => {
-			try {
-				const response = await fetch("https://api.themoviedb.org/3/trending/all/day?api_key=0544e732b88a2287b7288288339c30cf");
-				if (!response.ok) throw Error("Can't get the response");
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch(
+          "https://api.themoviedb.org/3/trending/all/day?api_key=0544e732b88a2287b7288288339c30cf"
+        );
+        if (!response.ok) throw Error("Can't get the response");
 
-				const json = await response.json();
-				setTopTrending(json.results.shift())
-				setTrending(json.results);
-			} catch (e) {
-				console.log(e);
-			}
-		})();
-	});
+        const json = await response.json();
+        setTopTrending(json.results.shift());
+        setTrending(json.results);
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  });
 
-	if (!topTrending) return <div>Loading</div>
+  if (!topTrending) return <div>Loading</div>;
 
-	return (
-		<>
-			<section className="">
-				<div className="bg-black flex flex-col items-center rounded-2xl h-fit font-Roboto p-4">
-					<a href={`/movie/${topTrending.id}`} className="relative mb-10">
-						<img className="rounded-xl w-full" src={`https://image.tmdb.org/t/p/w500${topTrending.backdrop_path}`} />
-						<div className="opacity-0 hover:opacity-100 hover:bg-black/[.7] rounded-xl duration-300 absolute inset-0 z-10 flex justify-center items-center">
-							<p className="text-xl text-white font-bold">
-								{topTrending.title || topTrending.name}
-							</p>
-						</div>
-					</a>
-					<div className="w-full">
-						<p className="text-xl text-white text-center mb-6">Now Trending</p>
-						<ul className="flex flex-col items-center h-72 overflow-x-hidden overflow-y-auto">
-							{trending.map((movie, index) => {
-								return (
-									<li key={index} className="mb-6">
-										<a href={`/movie/${movie.id}`} className="relative m-auto w-5/6">
-											<img className="rounded-xl" src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`} />
-											<div className="opacity-0 hover:opacity-100 hover:bg-black/[.7] rounded-xl duration-300 absolute inset-0 z-10 flex justify-center items-center">
-												<p className="text-xl text-white font-bold">
-													{movie.title || movie.name}
-												</p>
-											</div>
-										</a>
-									</li>
-								)
-							})}
-						</ul>
-					</div>
-				</div>
-			</section>
-		</>
-	)
-}
+  return (
+    <>
+      <section className="">
+        <div className="bg-black flex flex-col items-center rounded-2xl h-fit font-Roboto p-4">
+          <a href={`/movie/${topTrending.id}`} className="relative mb-10">
+            <img
+              className="rounded-xl w-full"
+              src={`https://image.tmdb.org/t/p/w500${topTrending.backdrop_path}`}
+            />
+            <div className="opacity-0 hover:opacity-100 hover:bg-black/[.7] rounded-xl duration-300 absolute inset-0 z-10 flex justify-center items-center">
+              <p className="text-xl text-white font-bold">
+                {topTrending.title || topTrending.name}
+              </p>
+            </div>
+          </a>
+          <div className="w-full">
+            <p className="text-xl text-white text-center mb-6">Now Trending</p>
+            <ul className="flex flex-col items-center h-72 overflow-x-hidden overflow-y-auto">
+              {trending.map((movie) => {
+                return (
+                  <li key={movie.id.toString()} className="mb-6">
+                    <a
+                      href={`/movie/${movie.id}`}
+                      className="relative m-auto w-5/6"
+                    >
+                      <img
+                        className="rounded-xl"
+                        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+                      />
+                      <div className="opacity-0 hover:opacity-100 hover:bg-black/[.7] rounded-xl duration-300 absolute inset-0 z-10 flex justify-center items-center">
+                        <p className="text-xl text-white font-bold">
+                          {movie.title || movie.name}
+                        </p>
+                      </div>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
